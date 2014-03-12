35611feb2f2b643dd2765e54c6e816e19d64d38d
leagueofcake <demonic997@hotmail.com>
2014-03-13 00:30:38 +1100

    Renamed Form1-->gameRewind (v2)

--- Rewind/My Project/Application.Designer.vb
+++ Rewind/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Rewind.Form1
+            Me.MainForm = Global.Rewind.gameRewind
         End Sub
     End Class
 End Namespace
