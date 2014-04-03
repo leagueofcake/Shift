218983ca786a42b1e8e39d2db5c1b9d47f36ad19
leagueofcake <demonic997@hotmail.com>
2014-04-03 18:38:10 +1100

    Shows "Paused" text when game is paused, other minor code fixes

--- Rewind/My Project/Resources.Designer.vb
+++ Rewind/My Project/Resources.Designer.vb
@@ -403,6 +403,16 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property pausedText() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("pausedText", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property projectile() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("projectile", resourceCulture)
