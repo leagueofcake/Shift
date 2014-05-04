9537433e8ca091904a6bfc4e3d2cbd140aebdccd
Dominic Fung <demonic997@hotmail.com>
2014-05-04 20:37:21 +1000

    Resized main screen to be same size as game screen, added blank help screen

--- Rewind/helpRewind.Designer.vb
+++ Rewind/helpRewind.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class mainRewind
+Partial Class helpRewind
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,32 +22,19 @@ Partial Class mainRewind
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.btnPlay = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
-        'btnPlay
-        '
-        Me.btnPlay.Location = New System.Drawing.Point(330, 197)
-        Me.btnPlay.Name = "btnPlay"
-        Me.btnPlay.Size = New System.Drawing.Size(75, 47)
-        Me.btnPlay.TabIndex = 0
-        Me.btnPlay.Text = "Play"
-        Me.btnPlay.UseVisualStyleBackColor = True
-        '
-        'mainRewind
+        'helpRewind
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackgroundImage = Global.Rewind.My.Resources.Resources.mainbg
         Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
         Me.ClientSize = New System.Drawing.Size(734, 441)
-        Me.Controls.Add(Me.btnPlay)
         Me.DoubleBuffered = True
-        Me.Name = "mainRewind"
-        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
-        Me.Text = "mainRewind"
+        Me.Name = "helpRewind"
+        Me.Text = "helpRewind"
         Me.ResumeLayout(False)
 
     End Sub
-    Friend WithEvents btnPlay As System.Windows.Forms.Button
 End Class
---
+++ Rewind/helpRewind.vb
@@ -0,0 +1,3 @@
+﻿Public Class helpRewind
+
+End Class
