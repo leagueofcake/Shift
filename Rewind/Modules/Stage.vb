a94b8900dbf0c70944eeb63b1c5d5e5dac35c5f2
Dominic Fung <demonic997@hotmail.com>
2014-05-10 21:09:55 +1000

    Updated stage spreadsheet, rearranged Stage class to fit order on the spreadsheet

--- Rewind/Modules/Stage.vb
+++ Rewind/Modules/Stage.vb
@@ -1,27 +1,27 @@
 ﻿Public Class Stage
     ' Game variables
-    Public Shared currentStage As Integer = 0
     Public Shared score As Integer = 0
-    Public Shared scoreMult As Integer = 0
     Public Shared genVar As Single
     Public Shared paused As Boolean = False
     Public Shared charge As Integer
-    Public Shared chargeGain As Integer = 5
-    Public Shared chargeMax As Integer = 500
+    Public Shared progression As Integer = 0 ' Essentially how long player has lasted in game
 
     ' Player variables
-    Public Shared playerSpeed As Integer = 4
     Public Shared playerHealth As Integer = 5000
     Public Shared shieldStatus As Integer = 0
-    Public Shared shieldMax As Integer = 100
-    Public Shared healthMax As Integer = 5000
 
     ' Difficulty shifting
+    Public Shared currentStage As Integer = 0
+    Public Shared playerSpeed As Integer = 4
+    Public Shared healthMax As Integer = 5000
+    Public Shared shieldMax As Integer = 100
     Public Shared healthDrain As Integer = 10
     Public Shared healthLoss As Integer = 20
     Public Shared healthGain As Integer = 100
+    Public Shared chargeGain As Integer = 5
+    Public Shared chargeMax As Integer = 500
     Public Shared projectileSpeed As Integer = 5
-    Public Shared progression As Integer = 0 ' Essentially how long player has lasted in game
+    Public Shared scoreMult As Integer = 0
 
     Public Shared Sub selectStage() ' Generate a stage number different to current stage
         Dim rand = New Random().Next(0, 3) ' 0, max no. stages + 1
@@ -34,35 +34,34 @@
 
     Public Shared Sub applyStage(stageNumber As Integer)
         Select Case stageNumber
-            ' shift(speed, chargeGain, chargeMax, shieldMax, healthMax, scoremult, healthDrain, healthLoss, healthGain, projectileSpeed)
+            ' shift(playerSpeed, healthMax, shieldMax, healthDrain, healthLoss, healthGain, chargeGain, chargeMax, projectileSpeed, scoreMult)
             Case 0 ' default
-                shift(4, 10, 500, 100, 5000, 100, 10, 10, 400, 5)
+                shift(4, 5000, 100, 10, 10, 400, 10, 500, 5, 100)
             Case 1 ' timeUp
-                shift(6, 5, 250, 50, 5000, 150, 10, 20, 600, 10)
+                shift(6, 5000, 50, 20, 20, 600, 5, 250, 10, 175)
                 ' Restart timers that were stopped by powerUp
                 gameRewind.timerGenerate.Enabled = True
                 gameRewind.timerWorld.Enabled = True
                 gameRewind.timerShield.Enabled = True
             Case 2 ' lowHealth
-                shift(4, 10, 500, 100, 2500, 200, 15, 20, 800, 5)
+                shift(4, 2500, 100, 20, 20, 800, 10, 500, 5, 200)
         End Select
 
         If charge > chargeMax Then charge = chargeMax ' Set charge to chargeMax if in switching stage chargeMax is lowered and charge > chargeMax
         If playerHealth > healthMax Then playerHealth = healthMax ' Set playerHealth to healthMax if in switching stage healthMax is lowered and playerHealth > healthMax
     End Sub
 
-    Public Shared Sub shift(speed As Integer, cGain As Integer, cMax As Integer, sMax As Integer, hMax As Integer, sMult As Integer, hDrain As Integer, hLoss As Integer, hGain As Integer, pSpeed As Integer)
+    Public Shared Sub shift(speed As Integer, hMax As Integer, sMax As Integer, hDrain As Integer, hLoss As Integer, hGain As Integer, cGain As Integer, cMax As Integer, pSpeed As Integer, sMult As Integer)
         playerSpeed = speed
-        chargeGain = cGain
-        chargeMax = cMax
-        shieldMax = sMax
         healthMax = hMax
-
-        scoreMult = sMult
+        shieldMax = sMax
         healthDrain = hDrain
         healthLoss = hLoss
         healthGain = hGain
+        chargeGain = cGain
+        chargeMax = cMax
         projectileSpeed = pSpeed
+        scoreMult = sMult
 
         gameRewind.picStage.BackgroundImage = My.Resources.ResourceManager.GetObject("stage" + currentStage.ToString)
     End Sub
